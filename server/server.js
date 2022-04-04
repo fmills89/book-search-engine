1f29bea447624a7e79759dcc1efb86c15669ae18
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-04-04 00:09:36 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-// import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
-
-// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const path = require("path");
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
@@ -28,14 +29,20 @@ const startServer = async () => {
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
-// Initialize the Apollo server
 startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
